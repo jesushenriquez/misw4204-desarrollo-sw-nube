8f6152af244ff2e65fceb0993d86090f9d38f75d
carloscastillof <c.castillof@uniandes.edu.co>
2023-10-18 21:26:07 -0500

    Taskmanager-test

--- TaskManager/app.py
+++ TaskManager/app.py
@@ -1,9 +1,19 @@
 import datetime
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 from celery import Celery
 import time
+import psycopg2 
+
+# Configura la conexión a la base de datos PostgreSQL
+db_connection = psycopg2.connect(
+    host="localhost",
+    port=5432,  # El puerto predeterminado de PostgreSQL
+    user="admin",
+    password="password",
+    database="cloud_db"
+)
 
 celery_app = Celery("informacionHvBuscadores", broker="redis://redis:6379/0")
 
@@ -22,7 +32,31 @@ def hello():
 
 @app.route(CONTEXT_PATH + TASKS_PATH, methods=["GET"])
 def getTasks():
-    return "Hello from" + CONTEXT_PATH + TASKS_PATH, 200
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM tasks")
+
+    tasks = cursor.fetchall()
+
+    cursor.close()
+
+    # Convierte los resultados en una lista de diccionarios
+    task_list = []
+    for task in tasks:
+        task_dict = {
+            'id': task[0],
+            'source_uuid': task[1],
+            'source_name': task[2],
+            'source_format': task[3],
+            'target_format': task[4],
+            'create_datetime': task[5],
+            'start_convert': task[6],
+            'end_convert': task[7],
+            'status': task[8]
+        }
+        task_list.append(task_dict)
+
+    return jsonify({"tasks": task_list}), 200
 
 
 if __name__ == "__main__":
