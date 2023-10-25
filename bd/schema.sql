6d0f34659dfbf8be930069c21bf592323d22e8be
Jesus Henriquez <ja.henriquez@uniandes.edu.co>
2023-10-25 00:24:39 -0500

    feature: update schema file

---
+++ bd/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username varchar(255),
+    password varchar(1000),
+    email varchar(1000)
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    source_uuid uuid NOT NULL,
+    source_name varchar(1000),
+    source_format varchar(5),
+    target_format varchar(5),
+    create_datetime timestamp,
+    start_convert timestamp,
+    end_convert timestamp,
+    status varchar(20),
+    user_id INT REFERENCES users(id)
+);
